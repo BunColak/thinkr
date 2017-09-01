43a12611225b336b8e2e1e9a1b643f2ab0665255
Bun Colak <bunyamincolak@gmail.com>
2017-09-01 20:54:26 +0300

    Added header and footer layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title="")
+    if page_title.empty?
+      full_title_name = "Thinkr."
+    else
+      full_title_name = page_title + " | Thinkr."
+    end
+  end
 end
