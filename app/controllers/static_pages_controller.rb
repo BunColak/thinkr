c99e80e55a5e63c4110ed3fbf2940b8555d6d985
BunColak <bunyamincolak@gmail.com>
2017-09-26 00:11:02 +0300

    fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @thought = current_user.thoughts.build if logged_in?
-    @thought_feed = current_user.feed.paginate(page: params[:page], per_page: 15)
+    @thought_feed = current_user.feed.paginate(page: params[:page], per_page: 15) if logged_in?
   end
 
   def help
