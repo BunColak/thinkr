0fcfa95e1684639beb1236046df0152d7ebd4788
BunColak <bunyamincolak@gmail.com>
2017-09-26 22:48:11 +0300

    fixes for google

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/google'                        # required
   config.fog_credentials = {
     provider:                         'Google',
-    google_storage_access_key_id:     'thinkr@oneall-site-432285-2.iam.gserviceaccount.com',
-    google_storage_secret_access_key: 'b386c0a18ab65e250eed3a9889ec9cecb141a183'
+    google_storage_access_key_id:     'thinkr@thinkr-181119.iam.gserviceaccount.com',
+    google_storage_secret_access_key: 'e3883d7c563422958ef6d2ac2e09c049bf8b4dda'
   }
   config.fog_directory = 'thought-pictures'
 end
